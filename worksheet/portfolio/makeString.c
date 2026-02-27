659631f96c75bf0696c98521da83692a4407603e
goyaltanishka <vmfl0704@leeds.ac.uk>
2026-02-27 12:02:19 +0000

    worksheet

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,13 +1,22 @@
 
 /*
-Name:
-Student ID:
+Name:Tanishka Goyal
+Student ID:201962648
 */
 
 #include <stdio.h>
 #include <string.h>
 
 int main( int argc, char **argv ) {
+    char buffer[100]=""; // the buffer is empty initially
+    for (int i=1; i<argc ; i++){ // we start with i=1 beacuse argc[0] is the program name 
+        strcat(buffer, argv[i]);
+        if(i<argc-1) {
+            strcat(buffer, "-");
+        }
+    }
+    
+
 
     // define appropriate data to hold your answer
 

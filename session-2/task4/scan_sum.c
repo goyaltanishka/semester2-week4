d865fbd651ccb9ce2226c08ec6316b43f5f06285
goyaltanishka <vmfl0704@leeds.ac.uk>
2026-02-28 22:36:48 +0000

    session2

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -2,7 +2,18 @@
 #include <stdio.h>
 
 int main( void ) {
+    int argument1;
+    int argument2;
+    int sum;
 
+    printf("Enter the first argument: ");
+    scanf(" %d\n",&argument1);
+    
+    printf("Enter the second argument:");
+    scanf(" %d\n",&argument2);
+
+    sum = argument1 + argument2 ;
+    printf("sum= %d\n",sum);
     // define suitable data
 
     // use scanf to read from the terminal
